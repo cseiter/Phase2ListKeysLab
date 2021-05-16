2d118c3b62dcbe2116ef64c895dc8afe4bab9c86
chris <cseiter@gmail.com>
2021-05-16 19:58:11 -0400

    ProjectList component updated to show projects

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,17 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  const showProjects = projects.map((indProject) => (
+    <ProjectItem key={indProject.id} 
+      name={indProject.name} 
+      about={indProject.about} 
+      technologies={indProject.technologies} />
+      )
+      );
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{showProjects}</div>
     </div>
   );
 }

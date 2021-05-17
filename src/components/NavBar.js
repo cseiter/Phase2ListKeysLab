e4809961f7e75196fd17355e22fe666695c964ce
chris <cseiter@gmail.com>
2021-05-16 20:12:18 -0400

    resolve issue with link syntax inside href

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
   const linksList = links.map((link) => {
-    return <a key={link} href="#{link}">{link}</a>;
+    return <a key={link} href={"#"+link}>{link}</a>;
   }); 
 
   return <nav>{linksList}</nav>;
